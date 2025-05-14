5af345a63935b178df3f5633b654e7a80e49d97b
Maulik-1911 <starmj24@gmail.com>
2025-05-14 13:28:47 +0530

    autologin implemented

---
+++ StuplexClient/hooks/useRefreshWithLoading.js
@@ -0,0 +1,28 @@
+import { useState,useEffect } from "react";
+import axios from 'axios';
+import { useDispatch, useDispatch } from "react-redux";
+import { setAuth } from "../store/authSlice";
+import {SERVER_URL} from '@env'
+export function useRefreshWithLoading(){
+    const [loading,setLoading]=useState(true);
+    const dispatch=useDispatch();
+    useEffect(()=>{
+        (async()=>{
+            try{
+                const data=await axios.get(
+                    `${SERVER_URL}/auth/refresh`,
+                    {
+                        withCredentials:true,
+                    }
+                );
+                dispatch(setAuth(data));
+                setLoading(false);
+            }catch(e){
+                console.log(e);
+                setLoading(false);
+            }
+        })
+    },[])
+
+    return {loading};
+}
